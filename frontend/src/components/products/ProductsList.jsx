63c1b7b85c35129ba0e0a2a87b18dab8f5cfac54
Marius <marius.sergent@gmail.com>
2024-02-26 12:13:30 +0100

    add product list for artisans

--- frontend/src/components/products/ProductsList.jsx
+++ frontend/src/components/products/ProductsList.jsx
@@ -1,7 +1,26 @@
-function ProductsList () {
-  return ( 
-    <h2>Product List</h2>
+import PropTypes from 'prop-types'
+import ProductsListItem from './ProductsListItem'
+import './ProductsList.css'
+/**
+ * 
+ * @param {Array} products 
+ * @returns {React.Component} ProductList
+ */
+function ProductsList ({ products }) {
+  if (!products || products.length < 1) return 'No data'
+  return (
+    <div>
+      {
+        products.map(product => (
+          <ProductsListItem key={product.id} product={product} />
+        ))
+      }
+    </div>
   ) 
+}
+
+ProductsList.propTypes = {
+  products: PropTypes.arrayOf(PropTypes.object)
 }
 
 export default ProductsList
--- frontend/src/components/products/ProductsListItem.jsx
+++ frontend/src/components/products/ProductsListItem.jsx
@@ -1,7 +1,24 @@
-function ProductsListItem () {
-  return ( 
-    <h2>Product List Item</h2>
+import PropTypes from 'prop-types'
+
+function ProductsListItem ({ product }) {
+  const { name, description, images } = product.attributes
+  const imgUrl = 'http://localhost:1337' + images?.data[0]?.attributes?.url
+  return (
+    <div className='card'>
+      <img
+        src={imgUrl}
+        className='product-picture'
+      />
+      <div className='card-body'>
+        <h3>{name}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
   )
+}
+
+ProductsListItem.propTypes = {
+  product: PropTypes.object.isRequired
 }
 
 export default ProductsListItem 
